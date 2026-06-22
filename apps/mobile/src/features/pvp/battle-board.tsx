1de07fb3799ac05e342cba9e43d23b54d6ffea7b
Zax-code <abouliatimz@yahoo.com>
2026-06-22 16:26:53 -0400

    Add space above opponent PvP units

--- apps/mobile/src/features/pvp/battle-board.tsx
+++ apps/mobile/src/features/pvp/battle-board.tsx
@@ -417,7 +417,7 @@ export function BattleBoard({
 
         <View className="relative z-10 flex-1 justify-center gap-2">
           <View className="relative min-h-0 flex-1 px-3 pt-1">
-            <View className="mb-8 flex-row items-center justify-center pl-[54px] pr-[110px]">
+            <View className="mb-12 flex-row items-center justify-center pl-[54px] pr-[110px]">
               <PlayerPlate
                 name={opponentPlayer.name}
                 energy={opponentPlayer.energy}
